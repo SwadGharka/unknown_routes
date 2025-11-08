e818f9205669b6d6f0c546f91354c42240a1c2b4
Mohit_Sahu <isdev03@seriindia.org>
2025-11-09 01:12:17 +0530

    some fix issues

--- src/main/java/com/v1/tourapp/entity/TourPackage.java
+++ src/main/java/com/v1/tourapp/entity/TourPackage.java
@@ -6,10 +6,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "tour_packages")
+@Table(name = "TOUR_PACKAGES")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,18 +22,24 @@ public class TourPackage {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID",  length = 2000)
     private Long id;
 
+    @Column(name="PACKAGE_NAME")
     private String packageName;
 
-    @Column(length = 2000)
+    @Column(name="DESCRIPTION", length = 2000)
     private String description;
 
+    @Column(name="PRICE")
     private Double price;
 
+    @Column(name="DURATION")
     private String duration;
 
+    @Column(name="IMAGE_URL")
     private String imageUrl;
 
+    @Column(name="LOCATION")
     private String location;
 }
